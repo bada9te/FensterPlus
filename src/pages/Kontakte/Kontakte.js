4bf1f8cf8a5115fcfdf6cab311f5cdad51f08d32
bohdan <bogdantelep@gmail.com>
2023-03-28 11:54:53 -0400

    kontakte goggle map

--- src/pages/Kontakte/Kontakte.js
+++ src/pages/Kontakte/Kontakte.js
@@ -1,11 +1,13 @@
 import { Container, Row, Col, Card } from "react-bootstrap";
 import ContactPerson from "../../components/contact-person/contact-person";
+import GoogleMap from "../../components/google-map/google-map";
+import BottomBar from '../../components/bottom-bar/bottom-bar';
 
 
 const Kontakte = props => {
     return (
         <>
-            <Container className='pt-5'>
+            <Container className='py-5'>
                 <Row>
                     <h2 className="text-center">Kontakte</h2>
                 </Row>
@@ -26,10 +28,30 @@ const Kontakte = props => {
                     </Col>
 
                     <Col>
-
+                        <Row className="p-3">
+                            <Card className="p-0">
+                                <Card.Header as="h5">WIR SIND HIER</Card.Header>
+                                <Card.Body>
+                                    <GoogleMap/>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                        <Row className="p-3">
+                            <Card className="p-0">
+                                <Card.Header as="h5">DIE ADRESSE</Card.Header>
+                                <Card.Body>
+                                    <Card.Text>Kiew, Akademiker Krimskogo 4a. Ukraine</Card.Text>
+                                    <Card.Text>Telefon: +38 067 502 19 90</Card.Text>
+                                    <Card.Text>in Deutschland: +49 151 506 13 12 6</Card.Text>
+                                    <Card.Text>verkaufde@glasso.ua</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
+
+            <BottomBar/>
         </>
     );
 }
