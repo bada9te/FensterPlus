1f765c1852bf652cff078e963292f5ddc2022e3f
bohdan <bogdantelep@gmail.com>
2023-03-29 12:57:43 -0400

    +server

--- client/src/components/CP/certificate/certificate-list.js
+++ client/src/components/CP/certificate/certificate-list.js
@@ -1,19 +1,31 @@
-import fileData from '../../../data/data.json';
+import certificates from '../../../data/certificates.json';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import Certificate from '../../certificate/certificate';
+import { useState } from 'react';
+//import FileSaver from 'file-saver'
 
 
 const CertificateList = props => {
+    const [certificatesData, setCertificatesData] = useState(certificates.data);
+
+    const handleRemove = (e) => {
+        const titleToRemove = e.target.getAttribute('name');
+        const newData = certificatesData.filter((item) => item.title !== titleToRemove)
+        setCertificatesData(newData);
+        console.log(certificatesData);
+    }
+
+
     return (
         <>
             <Container>
                 <Row>
                     {
-                        fileData.certificates.data.map((item, key) => {
+                        certificatesData.map((item, key) => {
                             return (
                                 <Col key={key}>
                                     <Card>
-                                        <Button title='Remove' variant='danger'>Remove</Button>
+                                        <Button title='Remove' variant='danger' name={item.title} onClick={handleRemove}>Remove</Button>
                                         <Certificate title={item.title} image={item.image}/>
                                     </Card>
                                 </Col>
--- client/src/pages/ControlPanel/ControlPanel.js
+++ client/src/pages/ControlPanel/ControlPanel.js
@@ -1,6 +1,5 @@
 import { Container, Row, Card } from 'react-bootstrap';
 import CertificateController from '../../components/CP/certificate/certificate-controller';
-import dataFile from '../../data/data.json';
 
 
 const ControlPanel = props => {
